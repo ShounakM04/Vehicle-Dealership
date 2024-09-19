e6a7586707d12bbe675c76e78b9bdf88fd84bd13
Yuvraj3006 <yuvr3006@gmail.com>
2024-09-19 19:40:05 +0530

    Changed .env

--- models/database.js
+++ models/database.js
@@ -1,11 +1,13 @@
 const pg = require("pg")
+require('dotenv').config();
+
 
 const db =  new pg.Client({
-    user: 'postgres',
-    host: 'localhost',
-    database:'cardealership',
-    password:'424354',
-    port:5432,
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database:process.env.DATABASE_NAME,
+    password:process.env.DATABASE_PASSWORD,
+    port:process.env.DATABASE_PORT,
 });
 
 db.connect();
