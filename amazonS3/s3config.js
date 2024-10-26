62c85defd0727dfb4c570ffb14708442c19798a2
Yuvraj3006 <yuvr3006@gmail.com>
2024-10-26 21:41:17 +0530

    Image fetched

---
+++ amazonS3/s3config.js
@@ -0,0 +1,37 @@
+const { S3Client, GetObjectCommand,  PutObjectCommand} =require ("@aws-sdk/client-s3");
+const  { getSignedUrl } =require ("@aws-sdk/s3-request-presigner");
+require('dotenv').config(); 
+
+const s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: process.env.AMAZON_ACCESS_KEY,
+        secretAccessKey: process.env.AMAZON_SECRET_KEY,
+    }
+});
+
+async function getObjectURL(key) {
+    const commmand = new GetObjectCommand({
+        Bucket: "cardealerbucket",
+        Key: key,
+    });
+    const url = await getSignedUrl(s3Client,commmand);
+    return url;
+}
+
+async function putObject(filename,filetype) {
+    const commmand =  new PutObjectCommand({
+        Bucket : "cardealerbucket",
+        Key: `uploads/${filename}`,
+        ContentType : filetype,
+    });
+    const url = await getSignedUrl(s3Client,commmand);
+    return url;
+}
+
+
+async function init() {
+    console.log("URL for image",await getObjectURL("audit_ppt.jpg"))
+}
+
+init();
