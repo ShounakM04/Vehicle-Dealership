cb46aabd8bbf24a27422240ae7486150c5f063aa
AmolPatil <134493844+AMOL29102@users.noreply.github.com>
2024-11-18 19:27:54 +0530

    delete notice fix

--- Backend/adminRoutes/dashboard.route.js
+++ Backend/adminRoutes/dashboard.route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const handleRecordDeletion = require("../controllers/deleteRecord.controller");
 const handleDashboard = require("../adminControllers/dashboard.controller");
 const { handleSellCar, getSoldCarDetails, getTotalSellingPrice } = require("../adminControllers/sellcar.controller")
-const { handleDeleteNotice, handleGetNotice } = require("../controllers/addNotice.controller")
 const {handleGetImages,handleDeleteImage}  = require("../controllers/s3ImageFunctions.controller")
 const { authenticateToken, authorizeEmployeeOrAdmin } = require("../controllers/userRole-auth");
 
@@ -12,7 +11,7 @@ router.get("/", authenticateToken, authorizeEmployeeOrAdmin, handleDashboard);
 router.post("/sell-car", authenticateToken, authorizeEmployeeOrAdmin, handleSellCar);
 router.get('/total-selling-price', getTotalSellingPrice);
 // router.get("/get-images", handleGetImages);
-router.delete("/delete-notice", authenticateToken, authorizeEmployeeOrAdmin, handleDeleteNotice)
+router.delete("/delete-notice", authenticateToken, authorizeEmployeeOrAdmin, handleDeleteImage)
 router.get("/sold-cars", authenticateToken, authorizeEmployeeOrAdmin, getSoldCarDetails)
 
 module.exports = router;
